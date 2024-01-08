279599aaba48b2e3cc3c0e170e4e62e6b97b9024
andrewwww2 <reyyreyy296@gmail.com>
2024-01-08 19:00:29 +0700

    menambahkan file

---
+++ loading-image.py
@@ -0,0 +1,12 @@
+# high pass filter ->
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread("sudoku (1).jpg", 0)
+
+laplacian = cv2.Laplacian(img, cv2.CV_64F)
+
+plt.imshow(laplacian, cmap="gray")
+plt.title("Laplacian"), plt.xticks([]), plt.yticks([])
+plt.show()
